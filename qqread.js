ac29653be8b07af9770f04d305b26e7c55c44bf4
Lexho1 <62320295+Lexho1@users.noreply.github.com>
2020-11-28 10:55:15 +0800

    Update qqread.js

--- qqread.js
+++ qqread.js
@@ -8,7 +8,7 @@ const notify = $.isNode() ? require('../sendNotify') : '';
 // 公共变量
 
 const Secrets = {
-    QQREAD_COOKIE: process.env.QQREAD_COOKIE,
+    COOKIE_QQYD: process.env.COOKIE_QQYD,
     PUSH_KEY: process.env.PUSH_KEY, //server酱推送消息
     BARK_PUSH: process.env.BARK_PUSH, //Bark推送
     TG_BOT_TOKEN: process.env.TG_BOT_TOKEN, //TGBot推送Token
